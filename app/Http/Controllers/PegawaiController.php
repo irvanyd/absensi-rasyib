2e4f0b47f2798ffc9aef641f3b8a20f7365a28e4
irvanyd <irvanyudha999@gmail.com>
2023-11-02 11:47:32 +0700

    add search and pagination on administrator page

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pegawai;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+
 class PegawaiController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $pegawai = DB::table('pegawai')->orderBy('nama_lengkap')
-        ->join('departemen', 'pegawai.kode_dept', '=','departemen.kode_dept')
-        ->get();
-        return view('pegawai.index', compact('pegawai'));
+        $query = Pegawai::query();
+        $query->select('pegawai.*', 'nama_dept');
+        $query->join('departemen', 'pegawai.kode_dept', '=', 'departemen.kode_dept');
+        $query->orderBy('nama_lengkap');
+        $pegawai = $query->paginate(3);
+        if (!empty($request->nama_lengkap)) {
+            $query->where('nama_lengkap', 'like', '%' . $request->nama_lengkap . '%');
+        }
+
+        if (!empty($request->kode_dept)) {
+            $query->where('pegawai.kode_dept', 'like', '%' . $request->kode_dept);
+        }
+        $pegawai = $query->paginate(3);
+
+
+        $departemen = DB::table('departemen')->get();
+        return view('pegawai.index', compact('pegawai', 'departemen'));
     }
 }
--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -19,43 +19,85 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>NUPTK</th>
-                                        <th>Nama</th>
-                                        <th>Jabatan</th>
-                                        <th>No. HP</th>
-                                        <th>Foto</th>
-                                        <th>Departemen</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($pegawai as $d)
-                                        @php
-                                            $path = Storage::url('uploads/pegawai/' . $d->foto);
-                                        @endphp
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $d->nuptk }}</td>
-                                            <td>{{ $d->nama_lengkap }}</td>
-                                            <td>{{ $d->jabatan }}</td>
-                                            <td>{{ $d->no_hp }}</td>
-                                            <td>
-                                                @if (empty($d->foto))
-                                                    <img src="{{ asset('assets/img/noprofile.jpeg') }}" class="avatar" alt="">
-                                                @else
-                                                    <img src="{{ url($path) }}" class="avatar" alt="">
-                                                @endif
-                                            </td>
-                                            <td>{{ $d->nama_dept }}</td>
-                                            <td></td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                            <div class="row">
+                                <div class="col-12">
+                                    <form action="/pegawai" method="GET">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <input type="text" name="nama_lengkap" id="nama_lengkap" 
+                                                    class="form-control" placeholder="Nama Karyawan" value="{{ Request('nama_lengkap') }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="form-group">
+                                                    <select name="kode_dept" id="kode_dept" class="form-select">
+                                                        <option value="">All Departemen</option>
+                                                        @foreach ($departemen as $d)
+                                                            <option {{ Request('kode_dept' )==$d->kode_dept ? 'selected' : ''}}
+                                                                value="{{ $d->kode_dept }}">{{ $d->nama_dept }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div class="form-group">
+                                                    <button class="input-group-text btn btn-primary">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+                                                            <path d="M21 21l-6 -6"></path>
+                                                         </svg>
+                                                        Search</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-12">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>NUPTK</th>
+                                                <th>Nama</th>
+                                                <th>Jabatan</th>
+                                                <th>No. HP</th>
+                                                <th>Foto</th>
+                                                <th>Departemen</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($pegawai as $d)
+                                                @php
+                                                    $path = Storage::url('uploads/pegawai/' . $d->foto);
+                                                @endphp
+                                                <tr>
+                                                    <td>{{ $loop->iteration + $pegawai->firstItem() - 1 }}</td>
+                                                    <td>{{ $d->nuptk }}</td>
+                                                    <td>{{ $d->nama_lengkap }}</td>
+                                                    <td>{{ $d->jabatan }}</td>
+                                                    <td>{{ $d->no_hp }}</td>
+                                                    <td>
+                                                        @if (empty($d->foto))
+                                                            <img src="{{ asset('assets/img/noprofile.jpeg') }}"
+                                                                class="avatar" alt="">
+                                                        @else
+                                                            <img src="{{ url($path) }}" class="avatar" alt="">
+                                                        @endif
+                                                    </td>
+                                                    <td>{{ $d->nama_dept }}</td>
+                                                    <td></td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            {{ $pegawai->links() }}
                         </div>
                     </div>
                 </div>
